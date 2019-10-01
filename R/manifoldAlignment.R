32f3adcb6b4cb411ba88925c0d49636ac6258b9a
Daniel Osorio <dcosorioh@tamu.edu>
2019-09-30 21:05:13 -0500

    manifoldAlignment added

--- R/manifoldAlignment.R
+++ R/manifoldAlignment.R
@@ -5,13 +5,13 @@ manifoldAlignment <- function(X, Y, d=3){
     if(!reticulate::py_module_available('numpy')){
       #reticulate::py_install('numpy')
     } else {
-      file <- system.file('python/nonLinearManifold.py', package = 'PCrTdMa')
-      reticulate::source_python(file)
+      file <- system.file('python/', package = 'PCrTdMa')
+      nonLinearManifold <- reticulate::import_from_path('nonLinearManifold', path = file)
       d <- as.integer(d)
       sharedGenes <- intersect(rownames(X), rownames(Y))
       X <- X[sharedGenes, sharedGenes]
       Y <- Y[sharedGenes, sharedGenes]
-      L <- Correspondence(matrix=diag(length(sharedGenes)))
+      L <- diag(length(sharedGenes))
       nonLinearManifold(X=X, Y=Y, corr=L, num_dims=d, Wx = X, Wy = Y)
     }
   }
