f855c31d8e83e45825a47da137c941ffc17b6720
dosorio <dcosorioh@tamu.edu>
2019-10-01 13:23:49 -0500

    linearManifold added

--- R/manifoldAlignment.R
+++ R/manifoldAlignment.R
@@ -1,6 +1,6 @@
 #' @export manifoldAlignment
 #' @importFrom reticulate py_available py_module_available import_from_path
-#' 
+#'
 #' @title manifoldAlignment
 #' @description ...
 #' @param X ...
@@ -10,19 +10,24 @@
 #' @references ...
 #' @details ...
 
-manifoldAlignment <- function(X, Y, d=3){
-  if(reticulate::py_available(initialize = TRUE)){
-    if(!reticulate::py_module_available('numpy')){
-      #reticulate::py_install('numpy')
-    } else {
-      file <- system.file('python/', package = 'PCrTdMa')
-      nonLinearManifold <- reticulate::import_from_path('nonLinearManifold', path = file)
-      d <- as.integer(d)
-      sharedGenes <- intersect(rownames(X), rownames(Y))
-      X <- X[sharedGenes, sharedGenes]
-      Y <- Y[sharedGenes, sharedGenes]
-      L <- diag(length(sharedGenes))
-      nonLinearManifold(X=X, Y=Y, corr=L, num_dims=d, Wx = X, Wy = Y)
-    }
+manifoldAlignment <- function(X, Y, d=3, method = 'nonLinear'){
+
+  file <- system.file('python/', package = 'PCrTdMa')
+
+  d <- as.integer(d)
+  sharedGenes <- intersect(rownames(X), rownames(Y))
+  X <- X[sharedGenes, sharedGenes]
+  Y <- Y[sharedGenes, sharedGenes]
+  L <- diag(length(sharedGenes))
+
+  if(method == 'nonLinear'){
+    nonLinearManifold <- reticulate::import_from_path('nonLinearManifold', path = file)
+    alignedNet <- nonLinearManifold(X = X, Y = Y, corr = L, num_dims = d, Wx = X, Wy = Y)
   }
+  if(method == 'Linear')
+    linearManifold <- reticulate::import_from_path('linearManifold', path = file)
+    alignedNet <- linearManifold(X=X, Y=Y, corr = L, num_dims = d, Wx = X, Wy = Y)
+  }
+
+  return(aligned)
 }
