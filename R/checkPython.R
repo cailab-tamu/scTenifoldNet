a14ba04eb840ab1bb94fc57e34cd3bac800c3213
dosorio <dcosorioh@tamu.edu>
2019-10-01 13:45:34 -0500

    checkPython added

---
+++ R/checkPython.R
@@ -0,0 +1,15 @@
+checkPython <- function() {
+  if (reticulate::py_available(initialize = TRUE)) {
+    pyPackages <- c('numpy', 'scipy', 'functools')
+    pyCheck <- sapply(pyPackages, function(X) {
+      reticulate::py_module_available(X)
+    })
+    if (all(pyCheck)) {
+      return(TRUE)
+    } else {
+      message(paste0(pyPackages[!pyCheck], ' not available'))
+    }
+  } else {
+    stop('Python not available')
+  }
+}
