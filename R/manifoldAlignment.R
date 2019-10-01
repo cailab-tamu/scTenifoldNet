cc9d4a184a9a3173a5a0304427e36759e46a1408
dosorio <dcosorioh@tamu.edu>
2019-10-01 15:00:09 -0500

    Update manifoldAlignment.R

--- R/manifoldAlignment.R
+++ R/manifoldAlignment.R
@@ -20,13 +20,23 @@ manifoldAlignment <- function(X, Y, d=3, method = 'nonLinear'){
     Y <- Y[sharedGenes, sharedGenes]
     L <- diag(length(sharedGenes))
     if(method == 'nonLinear'){
-      nonLinearManifold <- reticulate::import_from_path('nonLinearManifold', path = file)
-      alignedNet <- nonLinearManifold(X = X, Y = Y, corr = L, num_dims = d, Wx = X, Wy = Y)
+      netManifold <- reticulate::import_from_path(module = 'nonLinearManifold', path = file, convert = TRUE)
+      alignedNet <- netManifold$nonLinearManifold(X = X, Y = Y, corr = L, num_dims = d, Wx = X, Wy = Y)
     }
     if(method == 'Linear'){
-      linearManifold <- reticulate::import_from_path('linearManifold', path = file)
-      alignedNet <- linearManifold(X=X, Y=Y, corr = L, num_dims = d, Wx = X, Wy = Y)
+      netManifold <- reticulate::import_from_path(module = 'linearManifold', path = file, convert = TRUE)
+      alignedNet <- netManifold$LinearManifold(X=X, Y=Y, corr = L, num_dims = d, Wx = X, Wy = Y)
     }
     return(alignedNet)
   }
 }
+
+#
+# library(PCrTdMa)
+# X <- read.csv('https://raw.githubusercontent.com/namtk/ManiNetCluster/master/data/dayOrthoExpr.csv', row.names = 1)
+# X <- as.matrix(X)
+# Y <- read.csv('https://raw.githubusercontent.com/namtk/ManiNetCluster/master/data/nightOrthoExpr.csv', row.names = 1)
+# Y <- as.matrix(Y)
+# X <- as.matrix(PCrTdMa::pcNet(X, q = 0.95))
+# Y <- as.matrix(PCrTdMa::pcNet(Y, q = 0.95))
+# PCrTdMa::manifoldAlignment(X, Y, method = 'Linear')
