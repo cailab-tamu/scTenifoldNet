9c3c35787e333f5c1443ec7c2383567ab507a9b5
dosorio <dcosorioh@tamu.edu>
2019-10-03 10:46:42 -0500

    dCoexpression added

---
+++ R/dCoexpression.R
@@ -0,0 +1,14 @@
+#' @export dCoexpression
+#' @importFrom stats dist
+
+dCoexpression <- function(manifoldOutput, nGenes, geneList, q = 0.1){
+  dMetric <- sapply(seq_len(nGenes), function(G){
+    X <- manifoldOutput[G,]
+    Y <- manifoldOutput[(G+nGenes),]
+    I <- rbind(X,Y)
+    O <- dist(I)
+    O <- as.numeric(O)
+    return(O)
+  })
+  geneList[dMetric > quantile(dMetric, (1-q))]
+}
