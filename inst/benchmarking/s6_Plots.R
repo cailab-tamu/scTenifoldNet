90634210ceb0c10f8c0a2830cb23a5c795001828
Daniel Osorio <dcosorioh@tamu.edu>
2019-10-08 21:47:47 -0500

    Benchmarking updated

--- inst/benchmarking/s6_Plots.R
+++ inst/benchmarking/s6_Plots.R
@@ -10,31 +10,96 @@ SCC <- SCC[SCC$q == 0,]
 MI <- MI[MI$q == 0,]
 GENIE3 <- GENIE3[GENIE3$q == 0,]
 
-par(mfrow=c(1,2), mar=c(3,3,1,1), mgp=c(1.8,0.5,0))
-plot(PCR$nCells, PCR$acc, ylim=c(0.5,0.8), type='b',
-     ylab = 'Accuracy', xlab = 'Number of Cells', pch = 15)
-arrows(x0 = PCR$nCells, x1 = PCR$nCells, y0 = PCR$accLB, y1 = PCR$accUB,
-       length = 0.03, code = 3, angle = 90, col = 'black')
-points(SCC$nCells, SCC$acc, col = 'red', type='b', pch = 16)
-arrows(x0 = SCC$nCells, x1 = SCC$nCells, y0 = SCC$accLB, y1 = SCC$accUB,
-       length = 0.03, code = 3, angle = 90, col = 'red')
-points(MI$nCells, MI$acc, col = 'blue', type='b', pch = 17)
-arrows(x0 = MI$nCells, x1 = MI$nCells, y0 = MI$accLB, y1 = MI$accUB,
-       length = 0.03, code = 3, angle = 90, col = 'blue')
-points(GENIE3$nCells, GENIE3$acc, col = 'forestgreen', type='b', pch = 18)
-arrows(x0 = GENIE3$nCells, x1 = GENIE3$nCells, y0 = GENIE3$accLB, y1 = GENIE3$accUB,
-       length = 0.03, code = 3, angle = 90, col = 'forestgreen')
+library(RColorBrewer)
+pColors <- RColorBrewer::brewer.pal(4,'RdBu')
 
-plot(PCR$nCells, PCR$recall, ylim=c(0,1), type='b',
-     ylab = 'Accuracy', xlab = 'Number of Cells', pch = 15)
+png('figures/methodSelection.png', width = 1200*2, height = 1200, res = 300)
+par(mfrow=c(1,2), mar=c(3,3,1,1), mgp=c(1.8,0.5,0))
+plot(PCR$nCells, PCR$acc, ylim=c(0.5,0.8), type='b', col = pColors[1],
+     ylab = 'Accuracy', xlab = 'Number of Cells', pch = 15,)
+arrows(x0 = PCR$nCells, x1 = PCR$nCells, y0 = PCR$accLB, y1 = PCR$accUB,
+       length = 0.03, code = 3, angle = 90, col = pColors[1])
+points(SCC$nCells, SCC$acc, col = pColors[2], type='b', pch = 16)
+arrows(x0 = SCC$nCells, x1 = SCC$nCells, y0 = SCC$accLB, y1 = SCC$accUB,
+       length = 0.03, code = 3, angle = 90, col = pColors[2])
+points(MI$nCells, MI$acc, col = pColors[3], type='b', pch = 17)
+arrows(x0 = MI$nCells, x1 = MI$nCells, y0 = MI$accLB, y1 = MI$accUB,
+       length = 0.03, code = 3, angle = 90, col = pColors[3])
+points(GENIE3$nCells, GENIE3$acc, col = pColors[4], type='b', pch = 18)
+arrows(x0 = GENIE3$nCells, x1 = GENIE3$nCells, y0 = GENIE3$accLB, y1 = GENIE3$accUB,
+       length = 0.03, code = 3, angle = 90, col = pColors[4])
+legend('bottomright', legend = c('PCR','SCC', 'MI', 'GENIE3'), bty = 'n',
+       cex = 0.8, ncol = 2, pch = 15:18, col = pColors)
+
+plot(PCR$nCells, PCR$recall, ylim=c(0.15,1), type='b', col = pColors[1],
+     ylab = 'Recall', xlab = 'Number of Cells', pch = 15)
 arrows(x0 = PCR$nCells, x1 = PCR$nCells, y0 = PCR$recallLB, y1 = PCR$recallUB,
-       length = 0.03, code = 3, angle = 90, col = 'black')
-points(SCC$nCells, SCC$recall, col = 'red', type='b', pch = 16)
+       length = 0.03, code = 3, angle = 90, col = pColors[1])
+points(SCC$nCells, SCC$recall, col = pColors[2], type='b', pch = 16)
 arrows(x0 = SCC$nCells, x1 = SCC$nCells, y0 = SCC$recallLB, y1 = SCC$recallUB,
-       length = 0.03, code = 3, angle = 90, col = 'red')
-points(MI$nCells, MI$recall, col = 'blue', type='b', pch = 17)
+       length = 0.03, code = 3, angle = 90, col = pColors[2])
+points(MI$nCells, MI$recall, col = pColors[3], type='b', pch = 17)
 arrows(x0 = MI$nCells, x1 = MI$nCells, y0 = MI$recallLB, y1 = MI$recallUB,
-       length = 0.03, code = 3, angle = 90, col = 'blue')
-points(GENIE3$nCells, GENIE3$recall, col = 'forestgreen', type='b', pch = 18)
-arrows(x0 = GENIE3$nCells, x1 = GENIE3$nCells, y0 = GENIE3$recallLB, y1 = GENIE3$accUB,
-       length = 0.03, code = 3, angle = 90, col = 'forestgreen')
+       length = 0.03, code = 3, angle = 90, col = pColors[3])
+points(GENIE3$nCells, GENIE3$recall, col = pColors[4], type='b', pch = 18)
+arrows(x0 = GENIE3$nCells, x1 = GENIE3$nCells, y0 = GENIE3$recallLB, y1 = GENIE3$recallUB,
+       length = 0.03, code = 3, angle = 90, col = pColors[4])
+legend('bottomright', legend = c('PCR','SCC', 'MI', 'GENIE3'), bty = 'n',
+       cex = 0.8, ncol = 2, pch = 15:18, col = pColors)
+dev.off()
+
+
+PCR <- read.csv('metrics/PCR.csv')
+SCC <- read.csv('metrics/SCC.csv')
+MI <- read.csv('metrics/MI.csv')
+GENIE3 <- read.csv('metrics/GENIE3.csv')
+
+png('PR.png', width = 900*4,height = 900, res = 300)
+par(mfrow=c(1,4), mar=c(3,3,2,1), mgp=c(1.8,0.5,0))
+pColors <- RColorBrewer::brewer.pal(7,'RdBu')
+nCells <- unique(PCR$nCells)
+tPCR <- PCR[PCR$nCells == nCells[1],]
+plot(tPCR$recall,tPCR$acc, ylim=c(0.5,1), xlim = c(0,1), col=pColors[1], type = 'b',
+     xlab = 'Recall', ylab = 'Accuracy', pch=15, main = 'PCR')
+
+for(X in seq_along(nCells)[-1]){
+  tPCR <- PCR[PCR$nCells == nCells[X],]
+  points(tPCR$recall,tPCR$acc, ylim=c(0,1), col=pColors[X], type = 'b', pch=(15+(X-1)))
+}
+legend('bottomright', legend = paste0(nCells, ' Cells'), bty = 'n',
+       cex = 0.8, pch=15:22, col=pColors)
+
+
+tSCC <- SCC[SCC$nCells == nCells[1],]
+plot(tSCC$recall,tSCC$acc, ylim=c(0.5,1), xlim = c(0,0.9), col=pColors[1], type = 'b',
+     xlab = 'Recall', ylab = 'Accuracy', pch=15, main = 'SCC')
+
+for(X in seq_along(nCells)[-1]){
+  tSCC <- SCC[SCC$nCells == nCells[X],]
+  points(tSCC$recall,tSCC$acc, ylim=c(0,1), col=pColors[X], type = 'b', pch=(15+(X-1)))
+}
+legend('topright', legend = paste0(nCells, ' Cells'), bty = 'n',
+       cex = 0.8, pch=15:22, col=pColors)
+
+tMI <- MI[MI$nCells == nCells[1],]
+plot(tMI$recall,tMI$acc, ylim=c(0.5,1), xlim = c(0,0.5), col=pColors[1], type = 'b',
+     xlab = 'Recall', ylab = 'Accuracy', pch=15, main = 'MI')
+
+for(X in seq_along(nCells)[-1]){
+  tMI <- MI[MI$nCells == nCells[X],]
+  points(tMI$recall,tMI$acc, ylim=c(0,1), col=pColors[X], type = 'b', pch=(15+(X-1)))
+}
+legend('topright', legend = paste0(nCells, ' Cells'), bty = 'n',
+       cex = 0.8, pch=15:22, col=pColors)
+
+tGENIE3 <- GENIE3[GENIE3$nCells == nCells[1],]
+plot(tGENIE3$recall,tGENIE3$acc, ylim=c(0.5,1), xlim = c(0,0.7), col=pColors[1], type = 'b',
+     xlab = 'Recall', ylab = 'Accuracy', pch=15, main = 'GENIE3')
+
+for(X in seq_along(nCells)[-1]){
+  tGENIE3 <- GENIE3[GENIE3$nCells == nCells[X],]
+  points(tGENIE3$recall,tGENIE3$acc, ylim=c(0,1), col=pColors[X], type = 'b', pch=(15+(X-1)))
+}
+legend('topright', legend = paste0(nCells, ' Cells'), bty = 'n',
+       cex = 0.8, pch=15:22, col=pColors)
+dev.off()
