97a6c0401267c82486bdc9d9fe7d8dbd84237abc
dosorio <dcosorioh@tamu.edu>
2019-11-06 12:39:00 -0600

    scQC updated
    dgCMatrix support added

--- R/scQC.R
+++ R/scQC.R
@@ -2,10 +2,10 @@ scQC <- function(X){
   X[X < 0] <- 0
   lSize <- colSums(X)
   X <- X[,lSize > 1000]
-  lSize <- colSums(X)
+  lSize <- apply(X,2,sum)
   X <- X[,!lSize %in% boxplot.stats(lSize)$out]
   mtRate <- X[grepl('MT-',toupper(rownames(X))),]
-  mtRate <- colSums(mtRate)/colSums(X)
+  mtRate <- apply(mtRate,2,sum)/apply(X,2,sum)
   X <- X[,mtRate < 0.1]
   X <- as(X, 'dgCMatrix')
   return(X)
