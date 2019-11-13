3a509bccadedb1ce0bd17d0dbb491541cde136dd
dosorio <dcosorioh@tamu.edu>
2019-11-13 11:39:08 -0600

    Update scTenifoldNet.R

--- R/scTenifoldNet.R
+++ R/scTenifoldNet.R
@@ -18,11 +18,9 @@ scTenifoldNet <- function(X, Y, id, nNet = 10, nCells = 500, nComp = 3, symmetri
   
   X <- scTenifoldNet:::scQC(X)
   X <- scTenifoldNet:::CPM(X)
-  X <- X[apply(X!=0,1,mean) > 0.05,]
   
   Y <- scTenifoldNet:::scQC(Y)
   Y <- scTenifoldNet:::CPM(Y)
-  Y <- Y[apply(Y!=0,1,mean) > 0.05,]
   
   xNames <- rownames(X)
   yNames <- rownames(Y)
@@ -38,22 +36,28 @@ scTenifoldNet <- function(X, Y, id, nNet = 10, nCells = 500, nComp = 3, symmetri
   set.seed(1)
   yList <- makeNetworks(X = Y, nCells = nCells, nNet = nNet, nComp = nComp, scaleScores = scaleScores, symmetric = symmetric, q = (1-q))
   
-  for(M in c('I','3d','4d')){
-    set.seed(1)
-    tensorOut <- tensorDecomposition(xList, yList, d = 3, type = M)
-    Matrix::writeMM(tensorOut$X,paste0('X_',id,'_',M,'tensor.mtx'))
-    Matrix::writeMM(tensorOut$Y,paste0('Y_',id,'_',M,'tensor.mtx'))
-    writeLines(sharedGenes, paste0('genes_',id,'_',M,'tensor.mtx'))
-    tX <- as.matrix(tensorOut$X)
-    tY <- as.matrix(tensorOut$Y)
-    for(A in c('O','D','P')){
-      set.seed(1)
-      mA <- manifoldAlignment(tX , tY, d = 100, type = A)
-      rownames(mA) <- c(paste0('X_', sharedGenes),paste0('y_', sharedGenes))
-      outFile <-paste0(id,'_',M,'tensor_',A,'alignment.csv')
-      write.csv(mA, outFile)
-      dC <- dCoexpression(mA, length(sharedGenes), sharedGenes)
-      write.csv(dC, paste0('dCoex_',id,'_',M,'tensor_',A,'alignment.csv'))
-    }
-  }
+  # for(M in c('I','3d','4d')){
+  set.seed(1)
+  tensorOut <- tensorDecomposition(xList, yList, d = 3, type = M)
+  # Matrix::writeMM(tensorOut$X,paste0('X_',id,'_',M,'tensor.mtx'))
+  # Matrix::writeMM(tensorOut$Y,paste0('Y_',id,'_',M,'tensor.mtx'))
+  # writeLines(sharedGenes, paste0('genes_',id,'_',M,'tensor.mtx'))
+  tX <- as.matrix(tensorOut$X)
+  tY <- as.matrix(tensorOut$Y)
+  # for(A in c('O','D','P')){
+  set.seed(1)
+  mA <- manifoldAlignment(tX , tY, d = 30)
+  rownames(mA) <- c(paste0('X_', sharedGenes),paste0('y_', sharedGenes))
+  # outFile <-paste0(id,'_',M,'tensor_',A,'alignment.csv')
+  # write.csv(mA, outFile)
+  dC <- dCoexpression(mA, length(sharedGenes), sharedGenes)
+  # write.csv(dC, paste0('dCoex_',id,'_',M,'tensor_',A,'alignment.csv'))
+  # }
+  # }
+  outputResult <- list()
+  outputResult$Networks <- tensorOut
+  outputResult$ManifoldAlignment <- mA
+  outputResult$diffCoexpression <- dC
+  
+  return(outputResult)
 }
