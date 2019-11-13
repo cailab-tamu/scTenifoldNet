3c1b4cbde8a835aae1868d894b35de26bfff1eec
dosorio <dcosorioh@tamu.edu>
2019-11-13 11:00:15 -0600

    Update manifoldAlignment.R
    defaults added

--- R/manifoldAlignment.R
+++ R/manifoldAlignment.R
@@ -12,7 +12,7 @@
 #' @references ...
 #' @details ...
 
-manifoldAlignment <- function(X, Y, d=3, type = 'O'){
+manifoldAlignment <- function(X, Y, d=30){
   if(checkPyDependencies()){
     file <- system.file('python/', package = 'scTenifoldNet')
     d <- as.integer(d)
@@ -20,20 +20,20 @@ manifoldAlignment <- function(X, Y, d=3, type = 'O'){
     X <- X[sharedGenes, sharedGenes]
     Y <- Y[sharedGenes, sharedGenes]
     L <- diag(length(sharedGenes))
-    if(type == 'O'){
-      wX <- X
-      wY <- Y
-    }
-    if(type == 'D'){
-      wX <- X+1
-      wY <- Y+1
-    }
-    if(type == 'P'){
-      wX <- X
-      wY <- Y
-      wX[wX != 0] <- wX[wX != 0]+1
-      wY[wY != 0] <- wY[wY != 0]+1
-    }
+    # if(type == 'O'){
+    #   wX <- X
+    #   wY <- Y
+    # }
+    # if(type == 'D'){
+    wX <- X+1
+    wY <- Y+1
+    # }
+    # if(type == 'P'){
+    #   wX <- X
+    #   wY <- Y
+    #   wX[wX != 0] <- wX[wX != 0]+1
+    #   wY[wY != 0] <- wY[wY != 0]+1
+    # }
     netManifold <- reticulate::import_from_path(module = 'nonLinearManifold', path = file, convert = TRUE)
     alignedNet <- netManifold$nonLinearManifold(X = X, Y = Y, corr = L, num_dims = d, Wx = wX, Wy = wY)
     return(alignedNet)
