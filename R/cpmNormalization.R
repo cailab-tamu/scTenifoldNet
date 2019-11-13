1206a9a50ca4880537a7e20aeb6c0d72224d7917
dosorio <dcosorioh@tamu.edu>
2019-11-13 12:39:52 -0600

    Update cpmNormalization.R

--- R/cpmNormalization.R
+++ R/cpmNormalization.R
@@ -3,6 +3,7 @@
 #' @description Perfrom counts per million (CPM) normalization
 #' @param X ...
 #' @return ...
+
 cpmNormalization <- function(X){
   X <- as.matrix(X)
   X <- t(t(X)/colSums(X))*1e6
