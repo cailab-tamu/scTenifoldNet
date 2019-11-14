6b8fc26d4504094e7b02f06bf928626bbe8e29c3
Guanxun Li <victorguanxun@gmail.com>
2019-11-14 11:16:29 -0600

    Add variable description.

--- R/scTenifoldNet.R
+++ R/scTenifoldNet.R
@@ -3,46 +3,46 @@
 #' @importFrom methods as
 #' @importFrom rTensor as.tensor cp
 #' @description ...
-#' @param X ...
-#' @param Y ...
-#' @param qc_minLibSize ...
-#' @param qc_removeOutlierCells ...
-#' @param qc_minPCT ...
-#' @param qc_maxMTratio ...
-#' @param nNet ...
-#' @param nCells ...
-#' @param nComp ...
-#' @param symmetric ...
-#' @param scaleScores ...
-#' @param q ...
+#' @param X Raw counts matrix with cells as columns and genes (symbols) as rows.
+#' @param Y Raw counts matrix with cells as columns and genes (symbols) as rows.
+#' @param qc_minLibSize An integer value. Represents the minimum library size required for a cell to be included in the analysis.
+#' @param qc_removeOutlierCells A boolean value (TRUE/FALSE), if TRUE, the identified cells with library size greater than 1.58 IQR/sqrt(n) computed from the sample, are removed. For further details see: ?boxplot.stats
+#' @param qc_minPCT A decimal value between 0 and 1. Represents the minimum fraction of cells where the gene needs to be expressed to be included in the analysis.
+#' @param qc_maxMTratio A decimal value between 0 and 1. Represents the maximum ratio of mitochondrial reads (mithocondrial reads / library size) present in a cell to be included in the analysis. It's computed using the symbol genes starting with 'MT-'
+#' @param nNet The number of principal components regression based networks to generate.
+#' @param nCells The number of cells to subsample each time.
+#' @param nComp The number of principal components in PCA.
+#' @param symmetric A boolean value (TRUE/FALSE), if TRUE, the weights matrix returned will be symmetric.
+#' @param scaleScores A boolean value (TRUE/FALSE), if TRUE, the weights will be normalized such that the maximum absolute value is 1.
+#' @param q The threshold that only remaining the relationships with the top q% absolute value in the weights matrix.
 #' @return ...
 #' @references ...
 
 scTenifoldNet <- function(X, Y, qc_minLibSize = 1000, qc_removeOutlierCells = TRUE,
-                          qc_minPCT = 0.05, qc_maxMTratio = 0.1, nNet = 10, 
-                          nCells = 500, nComp = 3, symmetric = FALSE, scaleScores = TRUE, 
+                          qc_minPCT = 0.05, qc_maxMTratio = 0.1, nNet = 10,
+                          nCells = 500, nComp = 3, symmetric = FALSE, scaleScores = TRUE,
                           q = 0.05){
-  
+
   X <- scQC(X,minLibSize = qc_minLibSize, removeOutlierCells = qc_removeOutlierCells, minPCT = qc_minPCT, maxMTratio = qc_maxMTratio)
   X <- cpmNormalization(X)
-  
+
   Y <- scQC(Y)
   Y <- cpmNormalization(Y)
-  
+
   xNames <- rownames(X)
   yNames <- rownames(Y)
-  
+
   sharedGenes <- intersect(xNames, yNames)
   nGenes <- length(sharedGenes)
-  
+
   X <- X[sharedGenes,]
   Y <- Y[sharedGenes,]
-  
+
   set.seed(1)
   xList <- makeNetworks(X = X, nCells = nCells, nNet = nNet, nComp = nComp, scaleScores = scaleScores, symmetric = symmetric, q = (1-q))
   set.seed(1)
   yList <- makeNetworks(X = Y, nCells = nCells, nNet = nNet, nComp = nComp, scaleScores = scaleScores, symmetric = symmetric, q = (1-q))
-  
+
   # for(M in c('I','3d','4d')){
   set.seed(1)
   tensorOut <- tensorDecomposition(xList, yList, d = 3)
@@ -65,6 +65,6 @@ scTenifoldNet <- function(X, Y, qc_minLibSize = 1000, qc_removeOutlierCells = TR
   outputResult$Networks <- tensorOut
   outputResult$ManifoldAlignment <- mA
   outputResult$diffCoexpression <- dC
-  
+
   return(outputResult)
 }
