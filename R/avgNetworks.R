2c6c5671750ff01a7be0cb9865086c5cf0a0d860
dosorio <dcosorioh@tamu.edu>
2019-11-14 14:46:21 -0600

    Non used functions removed

--- R/avgNetworks.R
+++
@@ -1,8 +0,0 @@
-avgNetworks <- function(netList){
-  X <- netList[[1]]
-  for(i in seq_along(netList)[-1]){
-    X <- X + netList[[i]]
-  }
-  X <- X/length(netList)
-  return(X)
-}
--- R/filterNetwork.R
+++
@@ -1,6 +0,0 @@
-filterNetwork <- function(network, q = 0.95){
-  network <- as.matrix(network)
-  absNet <- abs(network)
-  network[absNet < quantile(absNet, q)] <- 0
-  return(network)
-}
