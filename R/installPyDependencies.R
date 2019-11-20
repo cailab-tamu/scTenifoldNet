f28f0b578060662d0653312d475fcb8563d5c8d9
dosorio <dcosorioh@tamu.edu>
2019-11-20 10:59:26 -0600

    Delete installPyDependencies.R

--- R/installPyDependencies.R
+++
@@ -1,9 +0,0 @@
-#' @export installPyDependencies
-#' @importFrom reticulate py_install
-#' @title Install \code{Phyton} dependencies
-#' @description Install the \code{numpy} and \code{scipy} \code{Phyton} packages using \code{reticulate}.
-installPyDependencies <- function(){
-  if(!checkPyDependencies()){
-    reticulate::py_install(c('numpy', 'scipy'))
-  }
-}
