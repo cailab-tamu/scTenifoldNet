a7768a228c3ae924caeb6b1a88cfa3347427cd3b
dosorio <dcosorioh@tamu.edu>
2020-01-03 11:45:21 -0600

    Annotations Upgraded

--- inst/manuscript/annotations.R
+++ inst/manuscript/annotations.R
@@ -2,7 +2,7 @@ library(enrichR)
 library(scTenifoldNet)
 
 nDim <- 30
-fileList <- list.files(path = 'results/',pattern = 'DF', full.names = TRUE)
+fileList <- list.files(path = 'results/',pattern = 'ANE', full.names = TRUE)
 
 sapply(fileList, function(X){
   fileContent <- read.csv(X, row.names = 1, stringsAsFactors = FALSE)
@@ -11,7 +11,7 @@ sapply(fileList, function(X){
   gList <- gList[grepl('X_', gList)]
   gList <- gsub('X_', '', gList)
   nGenes <- length(gList)
-  dC <- dCoexpression(fileContent)
+  dC <- dRegulation(fileContent)
   sGenes <- dC$gene[(dC$p.adj < 0.1)]
   sGenes <- toupper(sGenes)
   sGenes <- gsub('^MT-','',sGenes)
@@ -21,5 +21,5 @@ sapply(fileList, function(X){
   A <- do.call(rbind.data.frame, A)
   A <- A[A$P.value < 0.05,]
   A <- A[order(A$Adjusted.P.value),]
-  write.csv(A, file = paste0('annotations/A_',basename(X)))
+  write.csv(A, file = paste0('results/annotations/A_',basename(X)))
 })
