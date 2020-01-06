9b1ac9505bfd68189a53718547d34af1274188f8
dosorio <dcosorioh@tamu.edu>
2020-01-06 15:24:45 -0600

    Annotation updated

--- inst/manuscript/annotations.R
+++ inst/manuscript/annotations.R
@@ -2,7 +2,7 @@ library(enrichR)
 library(scTenifoldNet)
 
 nDim <- 30
-fileList <- list.files(path = 'results/',pattern = 'ANE', full.names = TRUE)
+fileList <- list.files(path = 'results/',pattern = 'DF', full.names = TRUE)
 
 sapply(fileList, function(X){
   fileContent <- read.csv(X, row.names = 1, stringsAsFactors = FALSE)
@@ -11,12 +11,13 @@ sapply(fileList, function(X){
   gList <- gList[grepl('X_', gList)]
   gList <- gsub('X_', '', gList)
   nGenes <- length(gList)
-  dC <- dRegulation(fileContent)
+  dC <- dRegulation(fileContent, minFC = 0.75)
   sGenes <- dC$gene[(dC$p.adj < 0.1)]
   sGenes <- toupper(sGenes)
   sGenes <- gsub('^MT-','',sGenes)
   writeLines(sGenes, paste0('geneLists/G_',basename(X)))
   plot(dC$Z, col=ifelse(dC$p.adj < 0.05,'red','black'), pch = 16)
+  plot(-log10(seq(0,1,length.out = nrow(dC))), -log10(dC$p.value))
   A <- enrichr(sGenes, c('KEGG_2019_Mouse', 'BioPlanet_2019', 'Reactome_2016'))
   A <- do.call(rbind.data.frame, A)
   A <- A[A$P.value < 0.05,]
