127c047d85a4924a7f74edde0783b73177a14ac1
Daniel Osorio <dcosorioh@tamu.edu>
2020-01-27 08:10:00 -0600

    new Annotations

--- inst/manuscript/annotations.R
+++ inst/manuscript/annotations.R
@@ -2,7 +2,7 @@ library(enrichR)
 library(scTenifoldNet)
 
 nDim <- 30
-fileList <- list.files(path = 'results/',pattern = 'DF', full.names = TRUE)
+fileList <- list.files(path = 'results/',pattern = 'non', full.names = TRUE)
 
 sapply(fileList, function(X){
   fileContent <- read.csv(X, row.names = 1, stringsAsFactors = FALSE)
@@ -11,8 +11,11 @@ sapply(fileList, function(X){
   gList <- gList[grepl('X_', gList)]
   gList <- gsub('X_', '', gList)
   nGenes <- length(gList)
-  dC <- dRegulation(fileContent, minFC = 0.75)
-  sGenes <- dC$gene[(dC$p.adj < 0.1)]
+  dC <- dRegulation(fileContent, minFC = 0)
+  dC$FC <- dC$distance^2/mean(dC$distance^2)
+  dC$p.value <- pchisq(dC$FC, df = 1, lower.tail = FALSE)
+  dC$p.adj <- p.adjust(dC$p.value, method = 'fdr')
+  sGenes <- dC$gene[(dC$p.adj < 0.05)]
   sGenes <- toupper(sGenes)
   sGenes <- gsub('^MT-','',sGenes)
   writeLines(sGenes, paste0('geneLists/G_',basename(X)))
@@ -20,7 +23,7 @@ sapply(fileList, function(X){
   plot(-log10(seq(0,1,length.out = nrow(dC))), -log10(dC$p.value))
   A <- enrichr(sGenes, c('KEGG_2019_Mouse', 'BioPlanet_2019', 'Reactome_2016'))
   A <- do.call(rbind.data.frame, A)
-  A <- A[A$P.value < 0.05,]
+  A <- A[A$Adjusted.P.value < 0.05,]
   A <- A[order(A$Adjusted.P.value),]
   write.csv(A, file = paste0('results/annotations/A_',basename(X)))
 })
