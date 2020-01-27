3d8e5808a8ab670730f24d7048598ea88340ca6b
Daniel Osorio <dcosorioh@tamu.edu>
2020-01-27 08:09:18 -0600

    new QQPlots

--- inst/manuscript/qqPlots.R
+++ inst/manuscript/qqPlots.R
@@ -10,8 +10,11 @@ sID <- c('Aging', 'DermalFibroblasts', 'Morphine', '-mtAging')
 sapply(seq_along(fileList), function(X){
   mA <- read.csv(fileList[X], row.names = 1)[,1:30]
   rownames(mA) <- make.unique((rownames(mA)))
-  dC <- dRegulation(mA, minFC = 0.75)
-  geneColor <- ifelse(dC$p.value < 0.01, 'red', 'black')
+  dC <- dRegulation(mA, minFC = 0)
+  dC$FC <- dC$distance^2/mean(dC$distance^2)
+  dC$p.value <- pchisq(dC$FC, df = 1, lower.tail = FALSE)
+  dC$p.adj <- p.adjust(dC$p.value, method = 'fdr')
+  geneColor <- ifelse(dC$p.adj < 0.1, 'red', 'black')
   if(sID[X] == 'Morphine'){
     gList <- c('Adcy5', 'Foxp1', 'Atp2b1', 'Ppp3ca', 'Rgs7bp', 'Ppp1r1b')
   }
@@ -23,25 +26,26 @@ sapply(seq_along(fileList), function(X){
   }
   
   geneColor[dC$gene %in% gList] <- 'forestgreen'
-  genePoint <- (ifelse(dC$p.adj < 0.1, 8, 16))
+  genePoint <- (ifelse(dC$p.adj < 0.05, 8, 16))
   geneID <- dC$gene
-  geneID[dC$p.adj > 0.1] <- ''
+  geneID[dC$p.adj > 0.05] <- ''
   
   tQ <- -log10(seq(0,1,length.out = nrow(dC)))
   sQ <- -log10(dC$p.value)
   dF <- data.frame(X= tQ, Y= sQ, geneID = geneID)
+  #dF <- data.frame(X= sort(rchisq(nrow(dC), df = 1), decreasing = TRUE), Y= dC$FC, geneID = geneID)
   rownames(dF) <- rownames(dC$gene)
   y <- -log10(dC$p.value)
-  y <- quantile(y, c(0.25,0.75))
-  x <- rev(-log10(qunif(c(0.25,0.75))))
+  y <- quantile(y, c(0.2,0.8))
+  x <- rev(-log10(qunif(c(0.2,0.8))))
   slope <- diff(y)/diff(x)
   int <- y[1L] - slope * x[1L]
   
-  png(paste0('figures/qq',sID[X],'.png'), width = 2100, height = 1500, res = 300, pointsize = 10)
+  png(paste0('figures/qq',sID[X],'.png'), width = 3150, height = 2250, res = 300, pointsize = 30)
   plotQQ <- ggplot(dF, aes(X,Y, label = geneID)) + 
     geom_point(color = geneColor, pch = genePoint) + 
     theme_bw() + 
-    geom_text_repel(segment.color = 'gray60', segment.alpha = 0.5, max.iter = 1e3, aes(fontface = ifelse(dC$gene %in% gList,2,1))) + 
+    geom_text_repel(segment.color = 'gray60', segment.alpha = 0.5, max.iter = 1e4, aes(fontface = ifelse(dC$gene %in% gList,2,1)), box.padding = .08) + 
     geom_abline(slope = slope, intercept = int, lty = 2) + labs(y=expression(-log[1*0]*" (Observed P-values)"),x=expression(-log[1*0]*" (Expected P-values)"))
   print(plotQQ)
   dev.off()
