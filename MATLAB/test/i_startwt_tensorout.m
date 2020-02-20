14d606d0b51438d3bc96a86a1037271a8f79212d
James Cai <jamescai@genomezoo.net>
2020-02-20 15:40:36 -0600

    Update i_startwt_tensorout.m

--- MATLAB/test/i_startwt_tensorout.m
+++ MATLAB/test/i_startwt_tensorout.m
@@ -1,5 +1,5 @@
-A0x=csvread('tensorOut_X.csv',1,1);
-A1x=csvread('tensorOut_Y.csv',1,1);
+A0=csvread('tensorOut_X.csv',1,1);
+A1=csvread('tensorOut_Y.csv',1,1);
 run('../s3_manifold_algn')
 [~,ind]=sort(vecnorm(aln0-aln1,2,2),'descend');
 ind(1:5)
