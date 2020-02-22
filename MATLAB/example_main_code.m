170c31ee31231c00b95f2ec125bb3374bae47cf5
James Cai <jamescai@genomezoo.net>
2020-02-21 21:14:50 -0600

    .

--- MATLAB/example_main_code.m
+++ MATLAB/example_main_code.m
@@ -2,9 +2,9 @@
 %[X,genelist]=sc_selectg(X,genelist,10,7);
 %[X]=sc_selectc(X,2000);
 load example10xdata_1877g X genelist
-%%
+
 rng default
-c=rand(size(X,2),1)>0.5;
+c=rand(size(X,2),1)>0;
 X0=X(:,~c);
 X1=X(:,c);
 X0=X1;
@@ -13,9 +13,29 @@ j=genelist=="CISD1";     % 80
 % s=X1(i,:); t=X1(j,:);
 % X1(i,:)=t; X1(j,:)=s;
 X1(i,:)=X1(j,:);
+
+[X0,genelist0]=sc_qcfilter(X0,genelist);
+[X1,genelist1]=sc_qcfilter(X1,genelist);
+[genelist,i,j]=intersect(genelist0,genelist1);
+X0=X0(i,:);
+X1=X1(j,:);
+ismember(["SWAP70","CISD1"],genelist)
+
 %%
 s1_network_constr;
-s2a_tensor_decomp;
+
+addpath('thirdparty\tensor_toolbox-v3.1\');
+for k=1:length(Ax0)
+    Xx0(:,:,k)=full(Ax0{k});
+end
+for k=1:length(Ax1)
+    Xx1(:,:,k)=full(Ax1{k});
+end
+
+%s2a_tensor_decomp;
+
+A0=i_tendecomp(Xx0,2);
+A1=i_tendecomp(Xx1,2);
 
 %{
 load('E:\OneDrive\outputs_scTenifoldNet\matlabtenorout.mat');
--- MATLAB/s1_network_constr.m
+++ MATLAB/s1_network_constr.m
@@ -20,7 +20,7 @@ X1=X1(j,:);
 % clearvars -except X0 X1 agegroup_ne genelist
 
 %%
-for k=1:5
+for k=1:10
     k
     tic    
     Xrep=X0(:,randperm(size(X0,2)));    
@@ -30,7 +30,7 @@ for k=1:5
 end
 
 %%
-for k=1:5
+for k=1:10
     k
     tic
     Xrep=X1(:,randperm(size(X1,2)));
@@ -38,9 +38,3 @@ for k=1:5
     Ax1{k}=sparse(A.*(abs(A)>quantile(abs(A(:)),0.95)));
     toc
 end
-
-
-
-
-
-
