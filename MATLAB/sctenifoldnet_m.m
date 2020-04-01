f298b77addbfe64dbc2a2601ecf846fa651751ad
James Cai <jamescai@genomezoo.net>
2020-03-31 23:25:29 -0500

    Update sctenifoldnet_m.m

--- MATLAB/sctenifoldnet_m.m
+++ MATLAB/sctenifoldnet_m.m
@@ -51,3 +51,11 @@ function T=sctenifoldnet_m(X0,X1,genelist,doplot)
     cd(pw0);
 end
 
+function txt = i_myupdatefcn1(~,event_obj,g)
+% Customizes text of data tips
+% pos = event_obj.Position;
+idx = event_obj.DataIndex;
+% i_plotsiglegene(idx,g);
+txt = {g(idx)};
+end
+
