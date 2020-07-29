e8489627ae8bf27287a8b84652e12e610ac695de
Daniel Osorio <dcosorioh@tamu.edu>
2020-07-29 14:05:03 -0500

    Update Dcomparison.R

--- inst/manuscript/Dcomparison.R
+++ inst/manuscript/Dcomparison.R
@@ -27,23 +27,26 @@ dE <- sapply(seq_along(sGenes), function(X){
   dist(rbind(A[X,],B[X,]))
 })
 names(dE) <- sGenes
-
+BC <- MASS::boxcox(dE~1)
+dE <- dE ^ abs(BC$x[which.max(BC$y)])
+zDE <- as.vector(scale(dE))
+names(zDE) <- names(dE)
 
 O <- read.csv('results/sym10X500morphineNeuron_Itensor_Dalignment.csv')
-Z <- O$distance
+Z <- O$Z
 names(Z) <- O$gene
 
-tGenes <- intersect(names(dE), names(Z))
+tGenes <- intersect(names(zDE), names(Z))
 
-DF <- data.frame(D=log1p(dE[tGenes]),MA=log10(Z[tGenes]))
+DF <- data.frame(D=(zDE[tGenes]),MA=(Z[tGenes]))
 
 A <- ggplot(DF, mapping = aes(D, MA)) +
   geom_point(col = densCols(DF$D, DF$MA, colramp = hcl.colors)) +
   theme_bw() +
-  xlab(expression(log(Expression~Distance + 1))) +
-  ylab(expression(log[10](Manifold~Distance))) +
+  xlab(expression(Z-score(Expression~Distance))) +
+  ylab(expression(Z-score(Manifold~Distance))) +
   labs(title = 'Expression Distance vs. Manifold Distance', subtitle = parse(text = paste0('rho == ', round(cor(DF$D, DF$MA, method = 'sp'),3)))) +
-  theme(plot.title = element_text(face = 2))
+  theme(plot.title = element_text(face = 2)) + coord_flip()
 
 png('DComparison.png', width = 1300, height = 1300, res = 300)
 A
@@ -72,23 +75,26 @@ dE <- sapply(seq_along(sGenes), function(X){
   dist(rbind(A[X,],B[X,]))
 })
 names(dE) <- sGenes
-
+BC <- MASS::boxcox(dE~1)
+dE <- dE ^ abs(BC$x[which.max(BC$y)])
+zDE <- as.vector(scale(dE))
+names(zDE) <- names(dE)
 
 O <- read.csv('results/sym10X500morphineNeuron_Itensor_Dalignment.csv')
-Z <- O$distance
+Z <- O$Z
 names(Z) <- O$gene
 
-tGenes <- intersect(names(dE), names(Z))
+tGenes <- intersect(names(zDE), names(Z))
 
-DF <- data.frame(D=log10(dE[tGenes]),MA=log10(Z[tGenes]))
+DF <- data.frame(D=(zDE[tGenes]),MA=(Z[tGenes]))
 
 A <- ggplot(DF, mapping = aes(D, MA)) +
   geom_point(col = densCols(DF$D, DF$MA, colramp = hcl.colors)) +
   theme_bw() +
-  xlab(expression(log[10](Tensor~Distance))) +
-  ylab(expression(log[10](Manifold~Distance))) +
-  labs(title = 'Tensor Distance vs. Manifold Distance', subtitle = parse(text = paste0('rho == ', round(cor(DF$D, DF$MA, method = 'sp'),3)))) +
-  theme(plot.title = element_text(face = 2))
+  xlab(expression(Z-score(Tensor~Distance))) +
+  ylab(expression(Z-score(Manifold~Distance))) +
+  labs(title = 'Expression Distance vs. Manifold Distance', subtitle = parse(text = paste0('rho == ', round(cor(DF$D, DF$MA, method = 'sp'),3)))) +
+  theme(plot.title = element_text(face = 2)) + coord_flip()
 
 png('NComparison.png', width = 1300, height = 1300, res = 300)
 A
