1863b00c98a8f377ef2b34a087a4555b84c1f48d
Daniel Osorio <dcosorioh@tamu.edu>
2020-07-29 09:01:54 -0500

    CCA

---
+++ inst/benchmarking/CCA/compareMethods.R
@@ -0,0 +1,25 @@
+library(Matrix)
+library(GGally)
+
+load('CCA_morphineNeuron.Rdata')
+MA <- read.csv('sym10X500morphineNeuron_Itensor_Dalignment.csv', row.names = 1)
+
+zMA <- MA$Z
+names(zMA) <- MA$gene
+
+zCCA2 <- dRCCA_d2$Z
+names(zCCA2) <- dRCCA_d2$gene
+
+zCCA3 <- dRCCA_d3$Z
+names(zCCA3) <- dRCCA_d3$gene
+
+zCCA4 <- dRCCA_d4$Z
+names(zCCA4) <- dRCCA_d4$gene
+
+sGenes <- intersect(names(zMA), names(zCCA2))
+sGenes <- intersect(sGenes, names(zCCA3))
+sGenes <- intersect(sGenes, names(zCCA4))
+
+ALL <- data.frame(MA=zMA[sGenes], CCA2 = zCCA2[sGenes], CCA3 = zCCA3[sGenes])
+rALL <- as.data.frame(apply(ALL,2,rank))
+ggpairs(rALL)
