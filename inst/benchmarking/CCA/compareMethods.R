1c8fbeac55539da2a439c1270d562973886abc7c
Daniel Osorio <dcosorioh@tamu.edu>
2020-07-29 12:51:19 -0500

    Update compareMethods.R

--- inst/benchmarking/CCA/compareMethods.R
+++ inst/benchmarking/CCA/compareMethods.R
@@ -16,10 +16,16 @@ names(zCCA3) <- dRCCA_d3$gene
 zCCA4 <- dRCCA_d4$Z
 names(zCCA4) <- dRCCA_d4$gene
 
+zCCA5 <- dRCCA_d5$Z
+names(zCCA5) <- dRCCA_d5$gene
+
 sGenes <- intersect(names(zMA), names(zCCA2))
 sGenes <- intersect(sGenes, names(zCCA3))
 sGenes <- intersect(sGenes, names(zCCA4))
 
-ALL <- data.frame(MA=zMA[sGenes], CCA2 = zCCA2[sGenes], CCA3 = zCCA3[sGenes])
+ALL <- data.frame(MA=zMA[sGenes], CCA2 = zCCA2[sGenes], CCA3 = zCCA3[sGenes], CCA4 = zCCA4[sGenes], CCA5 = zCCA5[sGenes])
 rALL <- as.data.frame(apply(ALL,2,rank))
+
+png('MA_CCA.png', width = 2000, height = 2000, res = 300)
 ggpairs(rALL)
+dev.off()
