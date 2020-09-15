8f84444d5e96ff219b0d1a95bfcf6e87e505776c
James Cai <jamescai@genomezoo.net>
2020-09-15 12:19:44 -0500

    Delete get_ribosomalgenes.m

--- MATLAB/private/get_ribosomalgenes.m
+++
@@ -1,9 +0,0 @@
-function [g]=get_ribosomalgenes
-options = weboptions('Timeout',21);
-websave('a.txt','https://www.genenames.org/cgi-bin/genegroup/download?id=1054&type=branch',options);
-% t=readtable('a.txt','PreserveVariableNames',true);
-warning off 
-t=readtable('a.txt','Range','A:B');
-warning on
-g=string(t.ApprovedSymbol);
-delete('a.txt');
