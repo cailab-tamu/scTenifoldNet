29ad0634d12fee74a1649f6fee30ee90e283fd03
James Cai <jamescai@genomezoo.net>
2020-11-17 15:03:39 -0600

    Update e_transf.m

--- MATLAB/e_transf.m
+++ MATLAB/e_transf.m
@@ -1,5 +1,13 @@
-function A=e_transf(A)
+function A=e_transf(A,q)
 % A - adjacency matrix
+if nargin<2, q=0.95; end
+dim=size(A);
+if numel(dim)==2
     A=A./max(abs(A(:)));
-    A=A.*(abs(A)>quantile(abs(A(:)),0.95));
+    A=A.*(abs(A)>quantile(abs(A(:)),q));
+elseif numel(dim)==3
+    for k=1:dim(3)
+        A(:,:,k)=e_transf(A(:,:,k),q);
+    end
+end
 end
