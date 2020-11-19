9ff0233446438156f0c37086e650ac6ad483306d
James Cai <jamescai@genomezoo.net>
2020-11-19 14:52:22 -0600

    Update e_transf.m

--- MATLAB/e_transf.m
+++ MATLAB/e_transf.m
@@ -3,8 +3,11 @@ function A=e_transf(A,q)
 if nargin<2, q=0.95; end
 dim=size(A);
 if numel(dim)==2
-    A=A./max(abs(A(:)));
-    A=A.*(abs(A)>quantile(abs(A(:)),q));
+    a=max(abs(A(:)));
+    if a>0
+        A=A./a;
+        A=A.*(abs(A)>quantile(abs(A(:)),q));        
+    end
 elseif numel(dim)==3
     for k=1:dim(3)
         A(:,:,k)=e_transf(A(:,:,k),q);
